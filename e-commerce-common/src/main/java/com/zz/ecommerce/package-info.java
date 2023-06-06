4b62562612b5065fb0db53e029cd168760f433db
zzunderwood <1042332293@qq.com>
2023-06-06 14:36:22 +0800

    first commit

---
+++ e-commerce-common/src/main/java/com/zz/ecommerce/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * @Author zhaozhen
+ * @Version 1.0
+ * @Date 2023/6/6 14:15
+ */
+package com.zz.ecommerce;
+//注释信息
---
+++ e-commerce-common/src/main/java/com/zz/ecommerce/vo/CommonResponse.java
@@ -0,0 +1,11 @@
+package com.zz.ecommerce.vo;
+
+/**
+ * 通用响应对象定义
+ * @Author zhaozhen
+ * @Version 1.0
+ * @Date 2023/6/6 14:17
+ */
+public class CommonResponse {
+
+}
