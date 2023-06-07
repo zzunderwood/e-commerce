fb706d7f6ea4c2c8f72975a6f2e6e6a9bfeab9b8
zzunderwood <1042332293@qq.com>
2023-06-07 16:25:08 +0800

    nacos配置

---
+++ e-commerce-alibaba-nacos-client/src/main/java/com/zz/ecommerce/controller/NacosClientController.java
@@ -0,0 +1,6 @@
+package com.zz.ecommerce.controller;/**
+ * @Author zhaozhen
+ * @Version 1.0
+ * @Date 2023/6/7 03:35
+*/public class NacosClientController {
+}
---
+++ e-commerce-alibaba-nacos-client/src/main/java/com/zz/ecommerce/service/NacosClientService.java
@@ -0,0 +1,6 @@
+package com.zz.ecommerce.service;/**
+ * @Author zhaozhen
+ * @Version 1.0
+ * @Date 2023/6/7 03:12
+*/public class NacosClientService {
+}
